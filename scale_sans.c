cd7fc3e4c6bd5f748c422e90ed7cb724c30d308d
Bart Massey <bart@cs.pdx.edu>
2018-11-24 02:30:35 -0800

    cleaned up stuff for redistribution

---
+++ scale_sans.c
@@ -0,0 +1,4 @@
+/* XXX Output produced automatically by glyphshades. */
+#include "glyphshades.h"
+/* Scale for Bitstream Vera Sans Mono. */
+char *scalechars_sans = "`._-',:~\";^!*/\\+()r<|>?l=ic[]tjvfzJs7x}1LY{CnTuyoIe2kF%wVh3aPSZ5Xmd$4EUbGA&qpg69OK08DH#QRW@BMN";
---
+++ scale_typewriter.c
@@ -0,0 +1,4 @@
+/* XXX Output produced automatically by glyphshades. */
+#include "glyphshades.h"
+/* Scale for Courier New. */
+char *scalechars_typewriter = "`.~^_'-,|:\\!/*}+{;=]<>()[7i1?l\"trvIc%jozJunCY&s3LVy5fx2k04aT6O9e$wSZhFPUDG8XmgAbpqdHK@R#EQBNWM";
